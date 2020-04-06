111ad7432dd8baee3a3f38f58775d6ca26f1a666
Kristjan <kristjan.kleimann@gmail.comgit config user.email kristjan.kleimann@gmail.com>
2020-04-06 12:29:08 +0300

    first upload

---
+++ src/main/java/Course.java
@@ -0,0 +1,12 @@
+public class Course {
+
+    String course_name = "";
+    int eap_amount;
+
+    public void course(String course_name, int eap_amount){
+
+        this.course_name = course_name;
+        this.eap_amount = eap_amount;
+    }
+}
+
---
+++ src/main/java/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+
+    String std_name = "";
+    int birth_year;
+    String takes_course;
+
+    public void student(String std_name, int birth_year, String takes_course){
+
+        this.std_name = std_name;
+        this.birth_year = birth_year;
+        this.takes_course = takes_course;
+    }
+}
