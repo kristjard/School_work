7a21ef344cf04dcfb85c2edc99fcb1bfcb79ec87
Kristjan <kristjan.kleimann@gmail.comgit config user.email kristjan.kleimann@gmail.com>
2020-05-01 17:01:58 +0300

    katse1

---
+++ src/main/java/data/Person.java
@@ -0,0 +1,13 @@
+import java.time.ZonedDateTime;
+
+
+public interface Person {
+    public void getFirstName(String name);
+    public void getLastName(String last_name);
+    public void getPreferredName(String preferred_name);
+    public void getDateOfBirth(String date_of_birth);
+    public void getAge(int age);
+    public void sayHello();
+
+
+}
